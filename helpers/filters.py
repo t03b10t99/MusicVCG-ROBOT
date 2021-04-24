0b5bcea4f2001258ac8bfe010b38fb9cd1fad7c9
Axel <80487896+KENZO-404@users.noreply.github.com>
2021-04-24 16:25:48 +0000

    Update filters.py

--- helpers/filters.py
+++ helpers/filters.py
@@ -1,7 +1,5 @@
 from typing import Union, List
-
 from pyrogram import filters
-
 from config import COMMAND_PREFIXES
 
 other_filters = filters.group & ~ filters.edited & ~ filters.via_bot & ~ filters.forwarded
