0fd05eba766a4572e12d56453dc89c9b30cbcdef
Axel <80487896+KENZO-404@users.noreply.github.com>
2021-04-24 13:21:20 +0000

    Create userbotjoin.py

---
+++ handlers/userbotjoin.py
@@ -0,0 +1,41 @@
+from callsmusic.callsmusic import client as USER
+from pyrogram import Client, filters
+from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram.errors import UserAlreadyParticipant
+from helpers.decorators import errors, authorized_users_only
+
+@Client.on_message(filters.group & filters.command(["userbotjoin"]))
+@authorized_users_only
+@errors
+async def addchannel(client, message):
+    chid = message.chat.id
+    try:
+        invitelink = await client.export_chat_invite_link(chid)
+    except:
+        await message.reply_text(
+            "<b>Tambahkan Saya Sebagai Admin Terlebih Dahulu.</b>",
+        )
+        return
+
+    try:
+        user = await USER.get_me()
+    except:
+        user.first_name =  "MusicVCG-ROBOT"
+
+    try:
+        await USER.join_chat(invitelink)
+    except UserAlreadyParticipant:
+        await message.reply_text(
+            "<b>Assistant Music Sudah Berada di Obrolan.</b>",
+        )
+        pass
+    except Exception as e:
+        print(e)
+        await message.reply_text(
+            f"<b>Mohon Maaf, User {user.first_name} Tidak Bergabung Dalam Group. Pastikan User Tidak Di Banned Dalam Group Ini."
+            "\n\nAtau Tambahkan Robot Dan Assistant Music Secara Manual kedalan Group Anda dan Coba Lagi.</b>",
+        )
+        return
+    await message.reply_text(
+            "<b>Assistant Music Baru Saja Bergabung Dengan Obrolan.</b>",
+        )
