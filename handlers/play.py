883c87dc25c106a293731817e805ca69292881d2
Dharma <77817262+KadekDharma@users.noreply.github.com>
2021-04-26 20:49:27 +0800

    TEST14##

--- handlers/play.py
+++ handlers/play.py
@@ -55,7 +55,7 @@ def cb_admin_check(func: Callable) -> Callable:
 
 
 def transcode(filename):
-    ffmpeg.input(filename).output("input.raw", format='s32le', acodec='pcm_s32le', ac=2, ar='48k').overwrite_output().run() 
+    ffmpeg.input(filename).output("input.raw", format='s16le', acodec='pcm_s16le', ac=2, ar='48k').overwrite_output().run() 
     os.remove(filename)
 
 # Convert seconds to mm:ss
