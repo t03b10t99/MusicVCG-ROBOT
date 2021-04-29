2317b449fd7dffee3bcd41b84997f3ecd06ad165
Axel <80487896+KENZO-404@users.noreply.github.com>
2021-04-29 11:33:38 +0000

    .
    Update :
    • CMD : /ping and  /uptime

--- handlers/start.py
+++ handlers/start.py
@@ -1,5 +1,8 @@
-from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+from time import time
+from datetime import datetime
+from pyrogram import Client, filters, emoji
+from pyrogram.types import Message
 
 from config import BOT_NAME as bn
 
@@ -86,4 +89,71 @@ async def repo(_, message: Message):
                 ]
             ]
         )
+    )
+
+# MusicVCG-ROBOT Ping and Info System
+
+
+
+START_TIME = datetime.utcnow()
+START_TIME_ISO = START_TIME.replace(microsecond=0).isoformat()
+TIME_DURATION_UNITS = (
+    ('week', 60 * 60 * 24 * 7),
+    ('day', 60 * 60 * 24),
+    ('hour', 60 * 60),
+    ('min', 60),
+    ('sec', 1)
+)
+
+self_or_contact_filter = filters.create(
+    lambda
+    _,
+    __,
+    message:
+    (message.from_user and message.from_user.is_contact) or message.outgoing
+)
+
+
+# https://gist.github.com/borgstrom/936ca741e885a1438c374824efb038b3
+async def _human_time_duration(seconds):
+    if seconds == 0:
+        return 'inf'
+    parts = []
+    for unit, div in TIME_DURATION_UNITS:
+        amount, seconds = divmod(int(seconds), div)
+        if amount > 0:
+            parts.append('{} {}{}'
+                         .format(amount, unit, "" if amount == 1 else "s"))
+    return ', '.join(parts)
+
+
+@Client.on_message(filters.text
+                   & self_or_contact_filter
+                   & ~filters.edited
+                   & ~filters.via_bot
+                   & filters.regex & filters.command("ping"))
+async def ping_pong(_, m: Message):
+    """reply ping with pong and delete both messages"""
+    start = time()
+    m_reply = await m.reply_text("...")
+    delta_ping = time() - start
+    await m_reply.edit_text(
+        f"{emoji.ROBOT} ping: `{delta_ping * 1000:.3f} ms`"
+    )
+
+
+@Client.on_message(filters.text
+                   & self_or_contact_filter
+                   & ~filters.edited
+                   & ~filters.via_bot
+                   & filters.regex & filters.command("uptime"))
+async def get_uptime(_, m: Message):
+    """/uptime Reply with readable uptime and ISO 8601 start time"""
+    current_time = datetime.utcnow()
+    uptime_sec = (current_time - START_TIME).total_seconds()
+    uptime = await _human_time_duration(int(uptime_sec))
+    await m.reply_text(
+        f"{emoji.ROBOT}\n"
+        f"• Uptime: `{uptime}`\n"
+        f"• Start Time: `{START_TIME_ISO}`"
     )
